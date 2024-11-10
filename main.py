78a454a46ccdd4b3553d0b7028e67ca88bc2712d
KevinRG19 <kerubianog@unal.edu.co>
2024-11-10 17:03:42 -0500

    Intento1

---
+++ main.py
@@ -0,0 +1,42 @@
+class Asiento:
+    def __init__(self, color, precio, registro):
+        self.color = color
+        self.precio = precio
+        self.registro = registro
+
+        def cambiarColor(self, nuevoColor):
+            listaColores = ["rojo", "verde", "amarillo", "negro", "blanco"]
+            if nuevoColor in listaColores:
+                self.color = nuevoColor
+
+
+class Motor:
+    def __init__(self, numeroCilindros, tipo, registro):
+        self.numeroCilindros = numeroCilindros
+        self.tipo = tipo
+        self.registro = registro
+
+    def cambiarRegistro(self, nuevoRegistro):
+        self.registro = nuevoRegistro
+
+    def asignarTipo(self, nuevoTipo):
+        if nuevoTipo == "gasolina" or nuevoTipo == "electrico":
+            self.tipo = nuevoTipo
+
+class Auto:
+    cantidadCreados = ""
+
+    def __init__(self, modelo, precio,asientos:list, marca, motor:Motor, registro):
+        self.modelo = modelo
+        self.precio = precio
+        self.asientos = asientos
+        self.marca = marca
+        self.motor = motor
+        self.registro = registro
+
+    def cantidadAsientos (self):
+        contador = 0
+        for i in range(len(self.asientos)):
+            if isinstance (self.asientos[i], Asiento):
+                contador = contador + 1
+        return contador
